9618df5ea68f730461e557b035855676a6be3b04
carlyle-felix <carlyle.felix@gmail.com>
2025-08-15 02:38:32 +0200

    allow setting of multiple keys

--- src/set_pi.c
+++ src/set_pi.c
@@ -7,7 +7,8 @@
 int main(int argc, char *argv[])
 {
     List l;
-    uint8_t i;
+    register uint8_t i;
+    uint8_t j;
     Key k;
 
     // check privilege
@@ -19,21 +20,28 @@ int main(int argc, char *argv[])
 
     if (argc == 1) {
         printf("menu goes here.\n");
-	return 0;
-    } else if (!strcmp(argv[1], "cpu")) {
-        k = ARM;
-    } else if (!strcmp(argv[1], "gpu")) {
-        k = GPU;
-    } else if (!strcmp(argv[1], "ov")) {
-        k = OV;
+        return 0;
     }
 
     l = create_list();
-    i = set_value(l, k, argv[2]);
-    if (i) {
-        printf("failed.\n");
-        return -1;
+    for (i = 1; i < argc; i++) {
+	if (!strcmp(argv[i], "cpu")) {
+            k = ARM;
+        } else if (!strcmp(argv[i], "gpu")) {
+            k = GPU;
+        } else if (!strcmp(argv[i], "ov")) {
+            k = OV;
+        } else {
+            continue;
+        }
+
+        j = set_value(l, k, argv[++i]);
+        if (j) {
+            printf("failed.\n");
+            return -1;
+        }
     }
+
     write_config(l);
     delete_list(l);
 
