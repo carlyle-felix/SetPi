4bbf9a925727f18aa4b692210e6150470ad2b763
carlyle-felix <carlyle.felix@gmail.com>
2025-08-15 02:02:17 +0200

    update set_pi.c

--- src/set_pi.c
+++ src/set_pi.c
@@ -8,6 +8,8 @@ int main(int argc, char *argv[])
 {
     List l;
     uint8_t i;
+    Key k;
+
     // check privilege
     if (!getuid())
     {
@@ -17,16 +19,23 @@ int main(int argc, char *argv[])
 
     if (argc == 1) {
         printf("menu goes here.\n");
-    } else if (!strcmp(argv[1], "arm_freq")) {
-        l = create_list();
-        i = set_value(l, ARM, argv[2]);
-        if (i) {
-            printf("failed.\n");
-            return -1;
-        }
-        write_config(l);
-        delete_list(l);
-    } 
+	return 0;
+    } else if (!strcmp(argv[1], "cpu")) {
+        k = ARM;
+    } else if (!strcmp(argv[1], "gpu")) {
+        k = GPU;
+    } else if (!strcmp(argv[1], "ov")) {
+        k = OV;
+    }
+
+    l = create_list();
+    i = set_value(l, k, argv[2]);
+    if (i) {
+        printf("failed.\n");
+        return -1;
+    }
+    write_config(l);
+    delete_list(l);
 
     return 0;
 }
