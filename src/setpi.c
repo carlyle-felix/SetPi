f1624efa6aef3d548d4620851b5d8e83e7eb7854
carlyle-felix <carlyle.felix@gmail.com>
2025-08-18 22:27:52 +0200

    update setpi.c
    add help and short options

--- src/setpi.c
+++ src/setpi.c
@@ -10,17 +10,26 @@ int main(int argc, char *argv[])
     List l;
     register int8_t i;
 
-    // check privilege
-    if (getuid())
-    {
-        printf("error: pi-freq needs root privileges.\n");
-        return -1;
-    }
+    if (argc == 1 || !strcmp(argv[1], "--help")) {
 
-    if (argc == 1) {
-        printf("menu goes here.\n");
+        printf("SetPi:\tA CLI program for managing Raspberry Pi's config.txt\n");
+        printf("Usage:\tsetpi [option] [<args>] ...\n");
+        printf("options:\n");
+        printf("\t--set | -s:\tadd or update config item(s).\n");
+        printf("\t--get | -g:\tprint current value of config item(s).\n");
+        printf("\n");
+        printf("example:\n");
+        printf("\tsetpi -s arm_freq=3000 gpu_freq=1000\n");
+
         return 0;
     } else {
+        // check privilege
+        if (getuid())
+        {
+            printf("error: pi-freq needs root privileges.\n");
+            return -1;
+        }
+
         l = create_list();
         for (i = 2; i < argc; i++) {
             l = add_item(l, argv[i]);
@@ -32,13 +41,13 @@ int main(int argc, char *argv[])
         }
     }
     
-    if (!strcmp(argv[1], "--set")) {
+    if (!strcmp(argv[1], "--set") || !strcmp(argv[1], "-s")) {
         i = write_config(l);
         if (i) {
             delete_list(l);
             return -1;
         }
-    } else if (!strcmp(argv[1], "--get")) {
+    } else if (!strcmp(argv[1], "--get") || !strcmp(argv[1], "-g")) {
         l = get_values(l);
         if (!l) {
             return -1;
